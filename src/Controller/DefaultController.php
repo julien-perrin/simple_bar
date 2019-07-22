4dfb50418ab16ecae38bcc99dd53467744b5f6d1
JULIEN PERRIN <JULIEN PERRIN>
2019-07-22 12:57:50 +0200

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+   
+    public function number($max = 9)
+    {
+        $number = random_int(0, $max);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+
+    public function dynamic($max = 9)
+    {
+        $number = random_int(0, $max);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number. $max .'</body></html>'
+        );
+    }
+
+    public function show_dynamic()
+    {
+        return new Response(
+            '<html><body>Dynamic2</body></html>'
+        );
+    }
+}
